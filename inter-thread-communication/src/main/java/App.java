d1a276ad1567c1c3dc5dd2e61606e5580d1f9640
Corneliu-Iosif Voiculescu <Corneliu-Iosif.Voiculescu@lseg.com>
2024-01-16 17:46:26 +0200

    Synchronized methods and blocks

---
+++ inter-thread-communication/src/main/java/App.java
@@ -0,0 +1,36 @@
+public class App {
+
+    public static Integer counter = 0;
+
+    public static final Object object1 = new Object();
+
+    public static void increment() {
+        synchronized (object1) {
+            counter++;
+        }
+    }
+
+    public static void process() throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+//                counter++;
+                increment();
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+//                counter++;
+                increment();
+            }
+        });
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println("Counter is: " + counter);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        process();
+    }
+}
